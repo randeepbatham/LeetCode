a9f4463073ad4e7ce60f5d212268690ae2a77d7f
Randeep Batham <randeepsinghbatham@gmail.com>
2025-08-19 15:56:09 +0530

    Create 148. Sort List.py

---
+++ Sorting/148. Sort List/148. Sort List.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head or not head.next:
+            return head
+
+        # step 1: split linked list into two halves
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+      
+        # step 2: Sort each half
+        left = self.sortList(head)
+        right = self.sortList(mid)
+      
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        tail.next = l1 if l1 else l2
+        return dummy.next
+            
+
+
+
+
+        
